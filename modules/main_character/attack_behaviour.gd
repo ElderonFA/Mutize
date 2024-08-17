9e5282e808dd69094e3b74ce00bf9e7e6020c7be
DESKTOP-4GE1PFI\zadon <zadonskiyilya@gmail.com>
2024-08-17 23:20:26 +0700

    Добавил атаку по кругу от игрока

--- modules/main_character/attack_behaviour.gd
+++ modules/main_character/attack_behaviour.gd
@@ -4,10 +4,16 @@ extends Behaviour
 @export var _projectile_scene: PackedScene
 @export var _projectiles_signals: ProjectilesSignals
 @export var _attack_point: Node2D
+@export var _attack_spawn_distance: float 
 
 
 func attack() -> void:
 	if not _is_active:
 		return
 	
-	_projectiles_signals.spawn_requested.emit(_attack_point.global_position, 3, _projectile_scene)
+	var mouse_pos = get_viewport().get_mouse_position()
+	var attack_vector = _attack_point.global_position - mouse_pos
+	var spawn_vector = _attack_point.global_position + attack_vector.normalized() * -_attack_spawn_distance
+	
+	_projectiles_signals.spawn_requested.emit(spawn_vector, Vector2.LEFT.angle_to(attack_vector), _projectile_scene)
+	
--- modules/projectiles_scenes/melee_projectile/melee_projectile.gd
+++ modules/projectiles_scenes/melee_projectile/melee_projectile.gd
@@ -2,6 +2,10 @@ extends Projectile
 
 
 @export var damage_count = 10
+@export var projectile_speed = 2
+
+func _physics_process(delta: float) -> void:
+	global_position += Vector2.RIGHT.rotated(global_rotation) * delta * projectile_speed
 
 
 func setup(rotation: float) -> void:
