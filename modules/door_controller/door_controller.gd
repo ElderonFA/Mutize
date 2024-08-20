e5903af6525ec884197a92c5cd08a9fee844f281
DESKTOP-4GE1PFI\zadon <zadonskiyilya@gmail.com>
2024-08-20 22:47:50 +0700

    Добавил открытие дверей после эволюции (рефактор)

---
+++ modules/door_controller/door_controller.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+@export var _character_signals: CharacterSignals
+@export var _animation_player: AnimationPlayer
+
+var is_open = false
+
+#нужен рефактор
+func _ready() -> void:
+	_character_signals.evolving_requested.connect(open_door)
+
+
+func open_door() -> void:
+	if is_open:
+		return
+		
+	is_open = true
+	_animation_player.play("open")
