9f46250e7ad14c49795cff15c2bb8463a448dbc4
DESKTOP-4GE1PFI\zadon <zadonskiyilya@gmail.com>
2024-08-25 20:27:09 +0700

    Рефактор механики поедания

--- modules/eat_behaviour/eat_behaviour.gd
+++ modules/eat_behaviour/eat_behaviour.gd
@@ -1,22 +1,20 @@
 extends Behaviour
 
-
+@export_group("Player nodes")
 @export var _animation_player: AnimationPlayer
-@export var _behaviours_for_onoff: Array[Behaviour]
+@export var _eating_area: Area2D
 @export var _eating_timer: Timer
-@export var _state_chart: StateChart
 
+@export var _state_chart: StateChart
 @export var _character_signals: CharacterSignals
+
+@export_group("Behaviors for on-off")
+@export var _behaviours_for_onoff: Array[Behaviour]
 
 var _current_corpse: Corpse
 
 
 func start_eating() -> void:
-	if _current_corpse == null:
-		_eating_timer.stop()
-		_state_chart.send_event("eating_ended")
-		return
-		
 	_animation_player.play("eating")
 	_eating_timer.start(_current_corpse.time_to_eat)
 	
@@ -32,22 +30,8 @@ func stop_eating() -> void:
 	for behaviour in _behaviours_for_onoff:
 		behaviour.activate()
 
-
-func _on_eating_area_area_entered(area: Area2D) -> void:
-	if area is Corpse:
-		_current_corpse = area
-
-
-func _on_eating_area_area_exited(area: Area2D) -> void:
-	if area is Corpse and _current_corpse == area:
-		_current_corpse = null
-
-
 #when corpse was eaten
-func _on_timer_timeout() -> void:
-	if _current_corpse == null:
-		return
-	
+func _on_timer_timeout() -> void:	
 	_character_signals.body_eaten.emit(_current_corpse.essence_count)
 	
 	_current_corpse.destroy_corpse()
@@ -56,8 +40,24 @@ func _on_timer_timeout() -> void:
 
 
 func _on_input_handler_eat_pressed() -> void:
+	_try_get_corpse()
+	
+	if _current_corpse == null:
+		return
+	
 	start_eating()
 
 
 func _on_input_handler_eat_released() -> void:
 	stop_eating()
+
+func _try_get_corpse() -> void:
+	var areas_in_eationg_area = _eating_area.get_overlapping_areas()
+	
+	for area in areas_in_eationg_area:
+		if area is Corpse:
+			if area.is_active:
+				_current_corpse = area
+				return
+	
+	_current_corpse = null
